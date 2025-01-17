fe2e08a366936c7629dd092c3454594decc0d152
iiinkston <1876364759@qq.com>
2025-01-17 20:33:26 +0800

    Initial commit

---
+++ src/components/CalorieCal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CalorieCal = () => {
+    return (
+        <div>
+            <h2>Calorie Calculator</h2>
+            <p>Track your daily calorie intake and manage your diet effectively.</p>
+        </div>
+    );
+};
+
+export default CalorieCal;
