9b2821d5a35732fbe93c716f876934dec2028633
OkJames <phankien555@gmail.com>
2025-02-22 12:17:38 +0800

    Modify data fetching functions from fetching from csv files to fetching from firebase

--- src/components/Cal.js
+++ src/components/Cal.js
@@ -7,296 +7,134 @@
 // Conger, S.A., Herrmann, S.D., Willis, E.A., Nightingale, T.E., Sherman, J.R., Ainsworth, B.E., 2024. 2024 Wheelchair Compendium of Physical Activities: An update of activity codes and energy expenditure values. Compendium of Physical Activities. Available at: https://pacompendium.com (Accessed: 21 January 2025).
 
 import { useEffect, useState } from "react";
-import * as XLSX from "xlsx";
+import { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
 
+// Fetch food data from firebase
 const useFoodData = () => {
-    const [foodData, setFoodData] = useState(null);
+    const [foodData, setFoodData] = useState([]);
 
     useEffect(() => {
-        const getData = async () => {
+        const fetchFoodData = async () => {
             try {
-                const response = await fetch("/food_calories.csv");
-                if (!response.ok) throw new Error("Failed to fetch file");
+                const foodCollection = collection(db, "calorie-cal", "food", "foodList");
+                const foodSnapshot = await getDocs(foodCollection);
+                const foodItems = [];
 
-                const text = await response.text();
+                foodSnapshot.forEach(doc => {
+                    const foodItem = doc.data();
+                    const portions = foodItem.portions;
 
-                const workbook = XLSX.read(text, { type: "string" });
+                    Object.keys(portions).forEach((portion) => {
+                        foodItems.push({
+                            value: `${foodItem.name}, ${portions[portion].portionAmount} ${portion}`,
+                            label: `${foodItem.name}, ${portions[portion].portionAmount} ${portion}`,
+                            cal: portions[portion].calories,
+                        });
+                    });
+                });
 
-                const sheet = workbook.Sheets[workbook.SheetNames[0]];
-                const data = XLSX.utils.sheet_to_json(sheet, { header: 1, defval: '' });
-
-                setFoodData(data);
+                setFoodData(foodItems);
             }
             catch (error) {
-                console.error("Error reading Excel file: ", error);
+                console.error("Error fetching food data:", error);
             }
         };
 
-        getData();
+        fetchFoodData();
     }, []);
-
     return foodData;
 };
 
+// Fetch activity data from firebase
+const useActivityData = () => {
+    const [activityData, setActivityData] = useState([]);
 
-// MET data to calculate calories burned for workouts
-const MET = {
-    Cycling: {
-        intensities: {
-            '< 19 km/h': 6.35,
-            '19.1 - 22.5 km/h': 9.17,
-            '> 22.6 km/h': 11,
-        }
-    },
-    Running: {
-        intensities: {
-            '< 6 km/h': 5.22,
-            '6 - 15 km/h': 10.17,
-            '> 15.1 km/h': 18.62,
-        }
-    },
-    Yoga: {
-        intensities: null,
-        metValue: 3.46
-    },
+    useEffect(() => {
+        const fetchActivityData = async () => {
+            try {
+                const activitiesCollection = collection(db, "calorie-cal", "activities", "activityList");
+                const activitiesSnapshot = await getDocs(activitiesCollection);
+                const activityItems = [];
 
-    Dancing: {
-        intensities: null,
-        metValue: 6.23
-    },
+                activitiesSnapshot.forEach(doc => {
+                    const activityItem = doc.data();
+                    const intensities = activityItem.intensities;
 
-    Batminton: {
-        intensities: {
-            'General': 5.5,
-            'Competitive': 8
-        }
-    },
+                    Object.keys(intensities).forEach((intensity) => {
+                        activityItems.push({
+                            value: `${doc.id}, ${intensity}`,
+                            label: `${doc.id} (${intensity})`,
+                            cal: intensities[intensity],
+                        });
+                    });
+                });
 
-    Basketball: {
-        intensities: {
-            'Game': 8,
-            'General': 6.75,
-            'Practice, drills': 9.3,
-            'Shooting baskets': 5
-        }
-    },
+                setActivityData(activityItems);
+            }
+            catch (error) {
+                console.error("Error fetching activity data:", error);
+            }
+        };
 
-    Bowling: {
-        intensities: null,
-        metValue: 3.4
-    },
-
-    Boxing: {
-        intensities: {
-            'Sparring': 7.8,
-            'Punching bag': 8.025,
-        }
-    },
-
-    Cheerleading: {
-        "Cheerleading, gymnastic moves, competitive": 6.0
-    },
-
-    Coaching: {
-        "football, soccer, basketball, baseball, swimming, etc.": 4.0,
-        'Playing with Players Actively': 8
-    },
-
-    Cricket: {
-        intensities: null,
-        metValue: 4.8
-    },
-
-    Curling: {
-        intensities: null,
-        metValue: 4
-    },
-
-    Fencing: {
-        intensities: {
-            'General': 6,
-            'Competitive': 9.8
-        }
-    },
-
-    Floorball: {
-        intensities: null,
-        metValue: 10.5
-    },
-
-    Football: {
-        intensities: {
-            "Competitive": 8,
-            "Light Effort": 4
-        }
-    },
-
-    Frisbee: {
-        intensities: {
-            "General": 3,
-            "Ultimate Frisbee": 8
-        },
-
-    },
-
-    FrisbeeGolf: {
-        intensities: null,
-        metValue: 3
-    },
-
-    Golf: {
-        intensities: null,
-        metValue: 4.06
-    },
-
-    Gymnastics: {
-        intensities: null,
-        metValue: 3.8
-    },
-
-    Handball: {
-        intensities: null,
-        metValue: 10
-    },
-
-    Hockey: {
-        intensities: {
-            'General': 8,
-            'Competitive': 10
-        },
-    },
-
-    HorsebackRiding: {
-        intensities: {
-            'General': 5.5,
-            "Trotting": 5.8,
-            "Walking": 3.8,
-            'Jumping': 9,
-            'Reining': 6
-        }
-    },
-
-    Taekwondo: {
-        intensities: null,
-        metValue: 14.3
-    },
-
-    Kendo: {
-        intensities: {
-            'kihon-keiko style, moderate intensity': 6.5,
-            'kirikaeshi style, high intensity': 9.6,
-            'kakari keiko style, very high intensity': 11.3
-        }
-    },
-
-    Judo: {
-        intensities: null,
-        metValue: 11.3
-    },
-
-    Kickboxing: {
-        intensities: null,
-        metValue: 7.3
-    },
-
-    Netball: {
-        intensities: null,
-        metValue: 7
-    },
-
-    Paddleball: {
-        intensities: {
-            'Casual': 6.0,
-            'Competitive': 10
-        }
-    },
-
-    RaceWalking: {
-        intensities: {
-            "3.1 m/s (6.9 mph)": 10.3,
-            "3.7 m/s (8.3 mph)": 13.8,
-            "4.0 m/s (8.95 mph)": 15.5
-        }
-    },
-
-    Racquetball: {
-        intensities: {
-            "General": 7.0,
-            "Competitive": 10
-        }
-    },
-
-    Climbing: {
-        intensities: {
-            'Rock or mountain climbing': 8,
-            'Rock climbing, free boulder': 8.8,
-            'Rock climbing, ascending rock, high difficulty': 7.3,
-            'Rock climbing, speed climbing, very difficult': 10.5,
-            'Rock climbing, ascending or traversing rock, low-to-moderate difficulty': 5.9,
-            'Rock climbing, treadwall, 7-10 m/min': 10.5,
-            'Rock climbing, rappelling': 5
-        }
-    },
-
-    RopeJumping: {
-        intensities: {
-            'fast pace, 120-160 skips/min': 12.3,
-            'moderate pace 100 - 120 skips/min': 11.8,
-            'slow pace, < 100 skips/min': 8.3
-        }
-    }
-
-
-
+        fetchActivityData();
+    }, []);
+    return activityData;
 };
 
-// Calculate calories burned for a workout
-const calculateCalories = (workoutType, time, intensityLevel, bodyWeight) => {
-    let metValue = 0;
-    if (MET[workoutType].intensities) {
-        metValue = MET[workoutType].intensities[intensityLevel] || 0;
-    } else {
-        metValue = MET[workoutType].metValue;
-    }
+// Calculate calories burned for a activity
+const calculateCalories = (metValue, time, bodyWeight) => {
     return (time * metValue * 3.5 * bodyWeight) / 200;
 };
 
-const handleWorkoutTimeChange = (e, setWorkoutTime, workout, intensity, bodyWeight, setCaloriesBurned) => {
+// Handle input time change
+const handleActivityTimeChange = (e, setActivityTime, activity, activityOptions, bodyWeight, setCaloriesBurned) => {
     const time = Number(e.target.value);
-    setWorkoutTime(time);
-    if (workout) {
-        const intensityLevel = intensity || null;
-        const calories = calculateCalories(workout, time, intensityLevel, bodyWeight);
-        setCaloriesBurned(calories);
+    setActivityTime(time);
+
+    if (activity) {
+        const selectedActivity = activityOptions.find(opt => opt.value === activity);
+        if (selectedActivity) {
+            const metValue = selectedActivity.cal;
+            const calories = calculateCalories(metValue, time, bodyWeight);
+            setCaloriesBurned(calories);
+        }
+        else {
+            setCaloriesBurned(0);
+        }
     }
 };
 
+// Handle selection of meal or activity
 const handleSelection = (type, setSelection, setShowPopup) => {
     setSelection(type);
     setShowPopup(true);
 };
 
-const closePopup = (setShowPopup, setWorkout, setIntensity) => {
+// Close the popup
+const closePopup = (setShowPopup, setActivity) => {
     setShowPopup(false);
-    setWorkout('');
-    setIntensity('');
+    setActivity('');
 };
 
+// Add meal selection
 const addMealSelection = (setMeals, meals) => {
     setMeals([...meals, ""]);
 };
 
+// Remove meal selection
 const removeMealSelection = (index, meals, setMeals) => {
     setMeals(meals.filter((_, i) => i !== index));
 };
 
+// Export functions
 export {
-    MET,
     calculateCalories,
-    handleWorkoutTimeChange,
+    handleActivityTimeChange,
     handleSelection,
     closePopup,
     useFoodData,
     addMealSelection,
-    removeMealSelection
+    removeMealSelection,
+    useActivityData
 };
